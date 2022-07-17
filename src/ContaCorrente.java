a9e1ad55836cd355f5c1128e6f3c6458a5381fc7
wilfoz <wil_foz@yahoo.com.br>
2022-07-16 21:25:47 -0300

    Initial commit

---
+++ src/ContaCorrente.java
@@ -0,0 +1,7 @@
+public class ContaCorrente extends Conta {
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfoComuns();
+    }
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,7 @@
+public class ContaPoupanca extends Conta {
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupanca ===");
+        super.imprimirInfoComuns();
+    }
+}
